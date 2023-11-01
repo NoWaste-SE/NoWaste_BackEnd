3328b2b43e66b30a6e7a649e5a0107473cbb50d1
“Negin <n.haghighy@gmail.com>
2023-11-01 14:01:38 +0330

    add: settings to connect postgresql on server

--- NoWaste/settings.py
+++ NoWaste/settings.py
@@ -94,9 +94,13 @@ ASGI_APPLICATION = "NoWaste.asgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'NoWaste',
+        'USER': 'gen',
+        'PASSWORD': 'gen39nowaste',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
