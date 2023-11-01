2402cbcf84a144c2828353f552f326942cabfce9
Golbarg <golbarg.sepehrara@gmail.com>
2023-11-01 19:38:11 +0330

    fix:change Access_Token_LifeTime

--- NoWaste/settings.py
+++ NoWaste/settings.py
@@ -184,6 +184,6 @@ STATICFILES_DIRS = [
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
