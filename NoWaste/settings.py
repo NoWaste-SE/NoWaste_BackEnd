084d1c03382376f0082d3f281fde7d8c654923b8
Melika Mohammadi <melikamfakhar@gmail.com>
2023-11-26 18:20:45 +0330

    feat: add environs

--- NoWaste/settings.py
+++ NoWaste/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from environs import Env
+
+# Environment Variables
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,11 +101,18 @@ ASGI_APPLICATION = "NoWaste.asgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nowaste',
-        'USER': 'gen',
-        'PASSWORD': 'gen39nowaste',
-        'HOST': 'db', 
-        'PORT': '5432',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),    
+        'PASSWORD': env.str('DB_PASSWORD'),  
+        'HOST': env.str('DB_HOST'), 
+        'PORT': '5432',  
+        
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'nowaste',
+        # 'USER': 'gen',
+        # 'PASSWORD': 'gen39nowaste',
+        # 'HOST': 'localhost', 
+        # 'PORT': '5432',
     }
 }
 
