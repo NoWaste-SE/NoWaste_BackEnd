cdce744b090b349f6bdf5af11391e233acb6be55
Golbarg <golbarg.sepehrara@gmail.com>
2023-12-01 12:50:35 +0330

    db setting

--- NoWaste/settings.py
+++ NoWaste/settings.py
@@ -108,8 +108,12 @@ ASGI_APPLICATION = "NoWaste.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'newview-1',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
